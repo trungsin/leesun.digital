78b1b55f3ca512712fbca01e343f767ccefa7749
Sin <sin.letrung@gmail.com>
2022-03-25 07:04:45 +0000

    d

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:5:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"F##$CZ#8?NYAT/MV^lO_Z[O$B(k=[wCR/92B8bPt2bZoQ|wtwX@#UP0UQ@}kR/Bu";s:7:"version";s:5:"1.0.4";s:13:"attackDataKey";i:3415;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"F##$CZ#8?NYAT/MV^lO_Z[O$B(k=[wCR/92B8bPt2bZoQ|wtwX@#UP0UQ@}kR/Bu";s:7:"version";s:5:"1.0.4";s:13:"attackDataKey";i:3415;s:11:"wafDisabled";b:0;}
