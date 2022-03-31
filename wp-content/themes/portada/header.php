ff2970e97dc5a50d35c07d708402f6b0ffa980ec
le sin <sin.letrung@gmail.com>
2022-03-31 15:03:17 +0700

    s

--- wp-content/themes/portada/header.php
+++ wp-content/themes/portada/header.php
@@ -41,12 +41,12 @@
 	document.addEventListener("DOMContentLoaded", function(){
   window.addEventListener('scroll', function() {
       if (window.scrollY > 50) {
-        document.getElementById('theme-header-menu').classList.add('fixed-top');
+        document.getElementsByClassName('theme-header-menu').classList.add('fixed-top');
         // add padding top to show content behind navbar
         navbar_height = document.querySelector('.navbar').offsetHeight;
         document.body.style.paddingTop = navbar_height + 'px';
       } else {
-        document.getElementById('theme-header-menu').classList.remove('fixed-top');
+        document.getElementsByClassName('theme-header-menu').classList.remove('fixed-top');
          // remove padding top from body
         document.body.style.paddingTop = '0';
       } 
