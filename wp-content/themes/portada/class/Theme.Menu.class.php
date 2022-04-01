69cd2e4d792549849151b5f4931139e4636a7a72
le sin <sin.letrung@gmail.com>
2022-04-01 14:49:44 +0700

    s

--- wp-content/themes/portada/class/Theme.Menu.class.php
+++ wp-content/themes/portada/class/Theme.Menu.class.php
@@ -109,67 +109,26 @@ class Portada_ThemeMenu
 		}
 		?>
 <style type="text/css">
-.overlay {
-  height: 100%;
-  width: 100%;
-  display: none;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: rgb(0,0,0);
-  background-color: rgba(0,0,0, 0.9);
+.form-control:focus, .form-control:active {
+    border-color: #000;
+    outline: 0;
+    box-shadow: inset 0 0 0;
 }
-
-.overlay-content {
-  position: relative;
-  top: 46%;
-  width: 80%;
-  text-align: center;
-  margin-top: 30px;
-  margin: auto;
-}
-
-.overlay .closebtn {
-  position: absolute;
-  top: 20px;
-  right: 45px;
-  font-size: 60px;
-  cursor: pointer;
-  color: white;
-}
-
-.overlay .closebtn:hover {
-  color: #ccc;
-}
-
-.overlay input[type=text] {
-  padding: 15px;
-  font-size: 17px;
-  border: none;
-  float: left;
-  width: 80%;
-  background: white;
-}
-
-.overlay input[type=text]:hover {
-  background: #f1f1f1;
-}
-
-.overlay button {
-  float: left;
-  width: 20%;
-  padding: 15px;
-  background: #ddd;
-  font-size: 17px;
-  border: none;
-  cursor: pointer;
-}
-
-.overlay button:hover {
-  background: #bbb;
-}
-</style>
+</style><div id="myOverlay" class="overlay">
+  <span class="closebtn" onclick="closeSearch()" title="Close Overlay">×</span>
+  <div class="overlay-content">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+  </div>
+</div>
+<script>
+$('#searchForm').on('shown.bs.collapse', function () {
+    // focus input on collapse
+    $("#search").focus()
+})
+</script>
 		<?php
 		$html=
 		'
@@ -183,7 +142,16 @@ class Portada_ThemeMenu
 						<a href="#"></a>
 						<img style="height:25px; width:108px; display:inline;" id="logomobile" src="'.esc_url(Portada_ThemeOption::getOption('header_logo_src')).'"/>
 						'.$htmlWooCommerce.'
-						'.$htmlSocialProfile.'
+						<div class="collapse fade" id="searchForm">
+                    <input id="search" type="search" class="form-control border-0" placeholder="search" />
+                </div>
+                <a class="nav-link ml-auto" 
+                    href="#searchForm" 
+                    data-target="#searchForm" 
+                    data-toggle="collapse">
+                    <i class="mdi mdi-magnify"></i>
+                </a>
+						
 					</div>
 					'.wp_nav_menu($menuResponsiveAttribute).'
 				</div>
