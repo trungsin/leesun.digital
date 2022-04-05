7dc4ad794f8505d9a1fed0997489165257a12116
le sin <sin.letrung@gmail.com>
2022-04-05 15:57:28 +0700

    s

--- wp-content/themes/portada/header.php
+++ wp-content/themes/portada/header.php
@@ -33,6 +33,10 @@
 		}
 ?>
 				<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+				 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 <?php				
 		wp_site_icon();
 		wp_head(); 
